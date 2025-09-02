05130e057142763148c0108097adf66f2945b399
jin <dbrwls12@gmail.com>
2025-09-02 17:53:46 +0900

    board3 게시판 수정 1.파일 첨부시 기존 파일 존재 유무에 따른 조건문 설정  기존 파일 없는 경우 추가  기존 파일 있는 경우에서 파일 변경시 기존 파일 삭제 후 새로운 파일  추가 2.게시판 삭제 기능 추가 3. 게시판 상세보기 페이지 추가

--- src/main/java/com/example/board/controller/BoardRestController.java
+++ src/main/java/com/example/board/controller/BoardRestController.java
@@ -2,11 +2,14 @@ package com.example.board.controller;
 
 
 import com.example.board.service.Board2Service;
+import com.example.board.service.Board3Service;
 import com.example.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class BoardRestController {
@@ -16,6 +19,9 @@ public class BoardRestController {
 
     @Autowired
     Board2Service board2Service;
+
+    @Autowired
+    Board3Service board3Service;
 
 
     @DeleteMapping(value = "deleteBoard/{BOARDID}")
@@ -28,5 +34,11 @@ public class BoardRestController {
     public String Board2(@PathVariable("id") Long id){
         board2Service.deleteByid(id);
         return "redirect:/board2";
+    }
+
+    @DeleteMapping(value = "delete3Board/{id}")
+    public String board3(@PathVariable("id") Long id) throws IOException {
+        board3Service.DeleteById(id);
+        return "redirect:/board3";
     }
 }
