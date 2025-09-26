d754527078611caec3ccb982881a8ecac33ece20
jin <dbrwls12@gmail.com>
2025-09-26 13:54:15 +0900

    authentication 상태 -> permitall로 변경뒤 좋아요 클릭시 발생되던 오류 해결

--- src/main/java/com/example/board/config/SecurityConfig.java
+++ src/main/java/com/example/board/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package com.example.board.config;
 
 
 import com.example.board.service.CustomOAuth2UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
 
 
 @EnableWebSecurity
@@ -24,7 +31,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -33,16 +40,25 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/css/**", "/js/**", "/images/**","/checkNickname", "/checkId", "/checkEmail", "/memberSave").permitAll()
-                        .requestMatchers( "/","/login", "/join", "/board1/**", "/board2/**", "/board3/**","/board4/**").permitAll()
-                        .requestMatchers( "/boardSave2", "/detail/{id}","/board5/**").permitAll()
-                        .requestMatchers("update4/{id}","/boardSave4","/delete4Board/{id}","update5Board/{id}",
-                                "/delete5Board/{id}","/boardSave" ).authenticated()
-                        .requestMatchers("/inputBoard","/inputBoard2","/inputBoard3","inputBoard4").authenticated()
-                        .requestMatchers( "/updateBoard", "/updateSave").authenticated()
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/checkNickname", "/checkId", "/checkEmail", "/memberSave", "/upload/**").permitAll()
+                        .requestMatchers("/", "/login", "/join", "/board1/**", "/board2/**", "/board3/**", "/board4/**").permitAll()
+                        .requestMatchers("/boardSave2", "/detail/{id}", "/board5/**", "/detail5Board/{id}").permitAll()
+                        .requestMatchers("/api/comments/**", "/api/detail5Board/{id}").permitAll()
+                        .requestMatchers("update4/{id}", "/boardSave4", "/delete4Board/{id}", "update5Board/{id}",
+                                "/delete5Board/{id}", "/boardSave").authenticated()
+                        .requestMatchers("/inputBoard", "/inputBoard2", "/inputBoard3", "inputBoard4", "/api/board5/like/{boardId}").authenticated()
+                        .requestMatchers("/updateBoard", "/updateSave").authenticated()
                         .anyRequest().authenticated()
                 )
-
+                .exceptionHandling(handling -> handling
+                        .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                            @Override
+                            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                                response.setContentType("text/plain;charset=UTF-8");
+                                response.getWriter().write("로그인 후 이용해주세요");
+                            }
+                        }))
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .loginProcessingUrl("/login")
@@ -51,12 +67,12 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/", true)
                         .failureUrl("/login?error=true")
                 )
-                .oauth2Login(oautch2 ->oautch2
+                .oauth2Login(oautch2 -> oautch2
                         .loginPage("/login")
-                        .userInfoEndpoint(userInfo->userInfo
-                                        .userService(customOAuth2UserService)
-                                )
-                        .defaultSuccessUrl("/",true)
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .defaultSuccessUrl("/", true)
                 )
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
@@ -65,8 +81,6 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-
-
 
 
 }
--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -40,21 +40,21 @@ public class BoardController {
 
 
     @GetMapping(value = "/board1")
-    public String board1(Model mo){
+    public String board1(Model mo) {
         List<BoardEntity> list = boardService.out();
-        mo.addAttribute("list",list);
+        mo.addAttribute("list", list);
         return "board1";
     }
 
-    @GetMapping(value ="/inputBoard" )
-    public String board2(){
+    @GetMapping(value = "/inputBoard")
+    public String board2() {
 
         return "inputBoard";
     }
 
     @PostMapping(value = "boardSave")
     public String board3(@RequestParam("title") String title, @RequestParam("author") String author,
-                         @RequestParam("content") String content, BoardDTO dto){
+                         @RequestParam("content") String content, BoardDTO dto) {
 
         dto.setTitle(title);
         dto.setAuthor(author);
@@ -66,16 +66,15 @@ public class BoardController {
     }
 
     @GetMapping(value = "updateBoard")
-    public String board4(@RequestParam("id") Long boardId, Model mo){
+    public String board4(@RequestParam("id") Long boardId, Model mo) {
         BoardEntity board = boardService.findout(boardId);
-        mo.addAttribute("board",board);
-        return"updateBoardView";
+        mo.addAttribute("board", board);
+        return "updateBoardView";
     }
 
     @PostMapping(value = "updateSave")
-    public String board5(@RequestParam("boardid") Long boardid,@RequestParam("title") String title,
-                         @RequestParam("content") String content,BoardDTO dto)
-    {
+    public String board5(@RequestParam("boardid") Long boardid, @RequestParam("title") String title,
+                         @RequestParam("content") String content, BoardDTO dto) {
         dto.setBOARDID(boardid);
         dto.setContent(content);
         dto.setTitle(title);
@@ -85,35 +84,35 @@ public class BoardController {
     }
 
     @GetMapping(value = "/board2")
-    public String board6(Model mo,@RequestParam(defaultValue = "1")int page){
+    public String board6(Model mo, @RequestParam(defaultValue = "1") int page) {
         Page<Board2DTO> boardList = board2Service.getBoardList(page);
 
-        int currentPage = boardList.getNumber()+1;
+        int currentPage = boardList.getNumber() + 1;
         int totalPages = boardList.getTotalPages();
         //블록 페이징
 
-        int pageSize=5;
-        int startPage = ((currentPage-1) / pageSize) * pageSize +1;
-        int endPage = Math.min(startPage + pageSize -1, totalPages);
+        int pageSize = 5;
+        int startPage = ((currentPage - 1) / pageSize) * pageSize + 1;
+        int endPage = Math.min(startPage + pageSize - 1, totalPages);
 
-        mo.addAttribute("boardList",boardList);
-        mo.addAttribute("currentPage",currentPage); //현재 페이지
-        mo.addAttribute("totalPages",totalPages);
-        mo.addAttribute("startPage",startPage);
-        mo.addAttribute("endPage",endPage);
+        mo.addAttribute("boardList", boardList);
+        mo.addAttribute("currentPage", currentPage); //현재 페이지
+        mo.addAttribute("totalPages", totalPages);
+        mo.addAttribute("startPage", startPage);
+        mo.addAttribute("endPage", endPage);
         // 총 페이지
         return "board2";
     }
 
     @GetMapping(value = "/inputBoard2")
-    public String board7(){
+    public String board7() {
 
         return "inputBoard2";
     }
 
     @PostMapping(value = "/boardSave2")
     public String board8(@RequestParam("title") String title, @RequestParam("content") String content
-                        , @RequestParam("author") String author, Board2DTO dto2){
+            , @RequestParam("author") String author, Board2DTO dto2) {
         dto2.setAuthor(author);
         dto2.setContent(content);
         dto2.setTitle(title);
@@ -127,65 +126,64 @@ public class BoardController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String board9(@PathVariable("id") Long id, Model mo){
+    public String board9(@PathVariable("id") Long id, Model mo) {
         Board2Entity data = board2Service.findById(id);
-        mo.addAttribute("data",data);
+        mo.addAttribute("data", data);
         return "board2Detail";
     }
 
     @GetMapping(value = "/update/{id}")
-    public String board10(@PathVariable("id") Long id, Model mo){
-        Board2Entity entity =board2Service.updateFindById(id);
-        mo.addAttribute("entity",entity);
+    public String board10(@PathVariable("id") Long id, Model mo) {
+        Board2Entity entity = board2Service.updateFindById(id);
+        mo.addAttribute("entity", entity);
         return "update2BoardView";
     }
 
     @PostMapping(value = "update2Save/{id}")
-    public String board11(@PathVariable("id") Long id, Board2DTO dto){
+    public String board11(@PathVariable("id") Long id, Board2DTO dto) {
         dto.setId(id);
         LocalDateTime now = LocalDateTime.now();
         dto.setInputDate(now);
-        board2Service.updateById(dto.getId(),dto.getTitle(),dto.getContent(),dto.getInputDate());
+        board2Service.updateById(dto.getId(), dto.getTitle(), dto.getContent(), dto.getInputDate());
 
         return "redirect:/board2";
     }
 
     @GetMapping(value = "/board3")
-    public String board12(Model mo,@RequestParam(defaultValue = "1")int page
-                            ,@RequestParam(required = false)String kw){
+    public String board12(Model mo, @RequestParam(defaultValue = "1") int page
+            , @RequestParam(required = false) String kw) {
         Page<Board3DTO> boardlist;
-        if(kw != null && !kw.trim().isEmpty()){
-            boardlist=board3Service.searchBoardList(kw,page);
-        }
-        else {
+        if (kw != null && !kw.trim().isEmpty()) {
+            boardlist = board3Service.searchBoardList(kw, page);
+        } else {
             boardlist = board3Service.getBoardList(page);
         }
 
-        int currentPage = boardlist.getNumber()+1;
+        int currentPage = boardlist.getNumber() + 1;
         int totalPages = boardlist.getTotalPages();
 
         int pageSize = 5;
-        int startPage = ((currentPage-1)/pageSize) * pageSize + 1 ;
-        int endPage = Math.min(startPage + pageSize -1,totalPages );
+        int startPage = ((currentPage - 1) / pageSize) * pageSize + 1;
+        int endPage = Math.min(startPage + pageSize - 1, totalPages);
 
-        mo.addAttribute("list",boardlist);
-        mo.addAttribute("currentPage",currentPage);
-        mo.addAttribute("totalPages",totalPages);
-        mo.addAttribute("startPage",startPage);
-        mo.addAttribute("endPage",endPage);
-        mo.addAttribute("kw",kw);
+        mo.addAttribute("list", boardlist);
+        mo.addAttribute("currentPage", currentPage);
+        mo.addAttribute("totalPages", totalPages);
+        mo.addAttribute("startPage", startPage);
+        mo.addAttribute("endPage", endPage);
+        mo.addAttribute("kw", kw);
         return "board3";
     }
 
     @GetMapping(value = "inputBoard3")
-    public String board13(){
+    public String board13() {
 
 
         return "inputBoard3";
     }
 
     @PostMapping(value = "boardSave3")
-    public String board14(Board3DTO board3DTO){
+    public String board14(Board3DTO board3DTO) {
 
         board3Service.saveBoard(board3DTO);
 
@@ -193,27 +191,27 @@ public class BoardController {
     }
 
     @GetMapping(value = "/update3/{id}")
-    public String board15(@PathVariable("id") Long id,Model mo){
+    public String board15(@PathVariable("id") Long id, Model mo) {
 
         Board3Entity entity = board3Service.findById(id);
-        mo.addAttribute("entity",entity);
+        mo.addAttribute("entity", entity);
 
         return "update3BoardView";
     }
 
     @PostMapping(value = "update3Save/{id}")
-    public String board16(@PathVariable("id")Long id,Board3DTO board3DTO ){
+    public String board16(@PathVariable("id") Long id, Board3DTO board3DTO) {
 
-        board3Service.UpdateBoard(board3DTO,id);
+        board3Service.UpdateBoard(board3DTO, id);
         return "redirect:/board3";
     }
 
     @GetMapping(value = "detail3/{id}")
-    public String board17(@PathVariable("id") Long id,Model mo){
-        Board3Entity entity =board3Service.findById(id);
+    public String board17(@PathVariable("id") Long id, Model mo) {
+        Board3Entity entity = board3Service.findById(id);
         log.info(entity.getFilePath());
         String filePath = entity.getFilePath();
-        if(filePath!=null) {
+        if (filePath != null) {
             String fileName = filePath.substring(filePath.lastIndexOf("\\") + 1);
             log.info(fileName);
             mo.addAttribute("fileName", fileName);
@@ -222,67 +220,71 @@ public class BoardController {
         mo.addAttribute("entity", entity);
         return "board3Detail";
     }
+
     //board4 부분
     @GetMapping(value = "/board4")
-    public String board18(Model mo){
-                List<Board4Entity> list = board4Service.out();
-                mo.addAttribute("list",list);
+    public String board18(Model mo) {
+        List<Board4Entity> list = board4Service.out();
+        mo.addAttribute("list", list);
         return "board4";
     }
 
     @GetMapping(value = "inputBoard4")
 
-    public String board19(){
+    public String board19() {
         return "inputBoard4";
     }
 
     @PostMapping(value = "boardSave4")
     public String board20(@AuthenticationPrincipal UserDetails userDetails, Board4DTO board4DTO) {
 
-            String author = userDetails.getUsername();
-            board4DTO.setAuthor(author);
-            board4Service.board4Save(board4DTO);
+        String author = userDetails.getUsername();
+        board4DTO.setAuthor(author);
+        board4Service.board4Save(board4DTO);
 
         return "redirect:/board4";
     }
+
     @GetMapping(value = "update4/{id}")
-    public String board21(@PathVariable("id") Long id,Model mo){
-            Board4Entity entity = board4Service.findById(id);
-            mo.addAttribute("entity",entity);
+    public String board21(@PathVariable("id") Long id, Model mo) {
+        Board4Entity entity = board4Service.findById(id);
+        mo.addAttribute("entity", entity);
         return "update4BoardView";
     }
-    @PostMapping(value = "/update4Save/{id}")
-    public String board22(@PathVariable("id")Long id,Board4DTO board4DTO){
 
-        board4Service.updateBoard(id,board4DTO);
-        return"redirect:/board4";
+    @PostMapping(value = "/update4Save/{id}")
+    public String board22(@PathVariable("id") Long id, Board4DTO board4DTO) {
+
+        board4Service.updateBoard(id, board4DTO);
+        return "redirect:/board4";
     }
+
     @GetMapping(value = "detail4/{id}")
-    public String board23(@PathVariable("id")Long id,Model mo){
+    public String board23(@PathVariable("id") Long id, Model mo) {
         Board4Entity entity = board4Service.findById(id);
         log.info(entity.getFilePath());
         String filePath = entity.getFilePath();
-        if(filePath!=null) {
+        if (filePath != null) {
             String fileName = filePath.substring(filePath.lastIndexOf("\\") + 1);
             log.info(fileName);
             mo.addAttribute("fileName", fileName);
 
         }
-        mo.addAttribute("entity",entity);
+        mo.addAttribute("entity", entity);
         return "board4Detail";
     }
+
     //board5 부분
     @GetMapping(value = "/board5")
-    public String board24(Model mo, @PageableDefault(size = 10,sort = "id")Pageable pageable,
-                          @RequestParam(required = false)String kw ){
+    public String board24(Model mo, @PageableDefault(size = 10, sort = "id") Pageable pageable,
+                          @RequestParam(required = false) String kw) {
         Page<Board5Entity> boardPage;
 //
-       if(kw !=null &&!kw.trim().isEmpty()){
-           boardPage= board5Service.searchBoardList(kw,pageable);
-       }
-       else{
-           boardPage=board5Service.findAll(pageable);
-       }
+        if (kw != null && !kw.trim().isEmpty()) {
+            boardPage = board5Service.searchBoardList(kw, pageable);
+        } else {
+            boardPage = board5Service.findAll(pageable);
+        }
 
         int currentPage = boardPage.getNumber() + 1;
         int pageSize = 5;
@@ -290,21 +292,21 @@ public class BoardController {
         int startPage = (currentBlock * pageSize) + 1;
         int endPage = Math.min(startPage + pageSize - 1, boardPage.getTotalPages());
 
-        mo.addAttribute("boardPage",boardPage);
-        mo.addAttribute("startPage",startPage);
-        mo.addAttribute("endPage",endPage);
-        mo.addAttribute("kw",kw);
+        mo.addAttribute("boardPage", boardPage);
+        mo.addAttribute("startPage", startPage);
+        mo.addAttribute("endPage", endPage);
+        mo.addAttribute("kw", kw);
         return "board5";
     }
 
     @GetMapping(value = "/inputBoard5")
-    public String board25(){
+    public String board25() {
 
         return "inputBoard5";
     }
 
     @PostMapping(value = "/boardSave5")
-    public String board26(Board5DTO board5DTO,@AuthenticationPrincipal UserDetails userDetails){
+    public String board26(Board5DTO board5DTO, @AuthenticationPrincipal UserDetails userDetails) {
         String author = userDetails.getUsername();
         board5DTO.setAuthor(author);
         board5Service.save(board5DTO);
@@ -313,24 +315,25 @@ public class BoardController {
     }
 
     @GetMapping(value = "/update5Board/{id}")
-    public String board27(@PathVariable("id")Long id,Model mo){
+    public String board27(@PathVariable("id") Long id, Model mo) {
 
         Board5Entity entity = board5Service.findByIdForUpdate(id);
-        mo.addAttribute("entity",entity);
+        mo.addAttribute("entity", entity);
 
         return "update5BoardView";
     }
-    @PostMapping(value = "/update5Save/{id}")
-    public String Board28(@PathVariable("id")Long id,Board5DTO board5DTO){
 
-        board5Service.updateBoard(id,board5DTO);
+    @PostMapping(value = "/update5Save/{id}")
+    public String Board28(@PathVariable("id") Long id, Board5DTO board5DTO) {
+
+        board5Service.updateBoard(id, board5DTO);
 
         return "redirect:/board5";
     }
 
     @GetMapping("/detail5Board/{id}")
-    public String board29(@PathVariable("id")Long id,Model mo){
-        mo.addAttribute("id",id);
+    public String board29(@PathVariable("id") Long id, Model mo) {
+        mo.addAttribute("id", id);
         return "board5Detail";
     }
 
